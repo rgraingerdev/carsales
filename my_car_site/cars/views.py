7314358da8bb7647be2fa7148e6f984780123021
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2024-02-12 16:57:32 +0000

    Basic CRUD func in place

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 
 def list(request):
-    # Your view logic here
-    return render(request, 'list.html')
+    all_cars = models.Car.objects.all()
+    context = {'all_cars' : all_cars}
+    return render(request, 'list.html', context=context)
 
 def add(request):
-    # Your view logic here
-    return render(request, 'add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand,year=year)
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'add.html')
 
 def delete(request):
-    # Your view logic here
-    return render(request, 'delete.html')
+    if request.POST:
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found!')
+    else:    
+        return render(request, 'delete.html')
